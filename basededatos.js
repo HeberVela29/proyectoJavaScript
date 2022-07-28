7b9c5560f9336ad1644b58d957b08e50ceb7b8e1
Heber Vela <vela.heber29@gmail.com>
2022-07-28 14:26:21 -0300

    JSON y Storage

---
+++ basededatos.js
@@ -0,0 +1,76 @@
+const zapatillas = [
+    {
+        id: "a1",
+        nombre: "Nike Cortez",
+        precio: 3000,
+        img: "./img/nikecortez.jpg"
+    },
+    {
+        id: "a2",
+        nombre: "Vans Old Skool",
+        precio: 2500,
+        img: "./img/vans.jpg"
+    },
+    {
+        id: "a3",
+        nombre: "Adidas Samba",
+        precio: 3000,
+        img: "./img/adidas.jpg"
+    },
+    {
+        id: "a4",
+        nombre: "New Balance 574",
+        precio: 2500,
+        img: "./img/newbalance.jpg"
+    }
+]
+
+const zapatos = [
+    {
+        id: "b1",
+        nombre: "Alden Indy Boots",
+        precio: 4500,
+        img: "./img/alden.jpg"
+    },
+    {
+        id: "b2",
+        nombre: "Corthay Arca Derby",
+        precio: 4000,
+        img: "./img/corthay.jpg"
+    },
+    {
+        id: "b3",
+        nombre: "John Lobb William II",
+        precio: 4500,
+        img: "./img/johnlobb.jpg"
+    }
+]
+
+const otros = [
+    {
+        id: "o1",
+        nombre: "Ojotas Lacoste",
+        precio: 1300,
+        img: "./img/lacoste.jpg"
+    },
+    {
+        id: "o2",
+        nombre: "Havaianas",
+        precio: 1000,
+        img: "./img/havaianas.jpg"
+    },
+    {
+        id: "o3",
+        nombre: "Crocs",
+        precio: 1200,
+        img: "./img/crocs.jpg"
+    },
+    {
+        id: "o4",
+        nombre: "Pantuflas Selú",
+        precio: 1200,
+        img: "./img/selu.jpg"
+    }
+]
+
+const listaProductos = zapatillas.concat(zapatos, otros)
--- main.js
+++ main.js
@@ -1,51 +1,145 @@
-// EVENTOS - SIMULADOR CARRITO
+// GENERANDO CARDS DE PRODUCTOS
 
-const botonAgregar = document.getElementsByClassName("agregar");
-console.log(botonAgregar)
-for(let boton of botonAgregar){
-    boton.addEventListener("click", obtenerDatos)
+// Zapatillas
+
+const mostrarZapatillas = (zapatillas) => {
+    const contenedorZapatillas = document.getElementById("contenedor-zapatillas");
+    zapatillas.forEach(product => {
+        const card = document.createElement("div");
+        card.innerHTML +=   
+                        `<div class="card" id="${product.id}" style="width: 18rem;">
+                            <img src="${product.img}" class="card-img-top" alt="">
+                            <div class="card-body">
+                                <h5 class="card-title">${product.nombre}</h5>
+                                <p class="card-text">$${product.precio}</p>
+                                <button class="btn btn-dark" id= "button${product.id}">Agregar al carrito</button>
+                            </div>
+                        </div>`
+        contenedorZapatillas.appendChild(card);
+
+        const button = document.getElementById(`button${product.id}`);
+        button.addEventListener("click", ()=> {
+             carrito(product.id);
+        })
+            
+    })
+}
+
+// Zapatos
+const mostrarZapatos = (zapatos) => {
+    const contenedorZapatos = document.getElementById("contenedor-zapatos");
+    zapatos.forEach(product => {
+        const card = document.createElement("div");
+        card.innerHTML +=   
+                        `<div class="card" id="${product.id}" style="width: 18rem;">
+                            <img src="${product.img}" class="card-img-top" alt="">
+                            <div class="card-body">
+                                <h5 class="card-title">${product.nombre}</h5>
+                                <p class="card-text">$${product.precio}</p>
+                                <button class="btn btn-dark" id= "button${product.id}">Agregar al carrito</button>
+                            </div>
+                        </div>`
+        contenedorZapatos.appendChild(card);
+
+        const button = document.getElementById(`button${product.id}`);
+        button.addEventListener("click", ()=> {
+             carrito(product.id);
+        })
+            
+    })
+}
+
+// Otros
+const mostrarOtros = (otros) => {
+    const contenedorOtros = document.getElementById("contenedor-otros");
+    otros.forEach(product => {
+        const card = document.createElement("div");
+        card.innerHTML +=   
+                        `<div class="card" id="${product.id}" style="width: 18rem;">
+                            <img src="${product.img}" class="card-img-top" alt="">
+                            <div class="card-body">
+                                <h5 class="card-title">${product.nombre}</h5>
+                                <p class="card-text">$${product.precio}</p>
+                                <button class="btn btn-dark" id= "button${product.id}">Agregar al carrito</button>
+                            </div>
+                        </div>`
+        contenedorOtros.appendChild(card);
+
+        const button = document.getElementById(`button${product.id}`);
+        button.addEventListener("click", ()=> {
+             carrito(product.id);
+        })
+            
+    })
 }
 
 
-// Obteniendo datos del nodo
-function obtenerDatos (e) {
-    let boton = e.target;
-    let producto = boton.parentElement;
-    
-    let prodName = producto.querySelector(".nombre").innerText;
-    let prodPrice = producto.querySelector(".precio").innerText;
-    let imagen = producto.parentElement;
-    let prodID = imagen.getAttribute("id");
-    let prodImg = imagen.querySelector(".imagen").src;
-    agregarCarrito(prodID, prodName, prodPrice, prodImg)
+mostrarZapatillas(zapatillas);
+mostrarZapatos(zapatos);
+mostrarOtros(otros);
+
+
+// SIMULADOR CARRITO
+
+const carritoContenedor = document.getElementById("product-rows")
+
+// APLICANDO JSON y STORAGE
+function capturarStorage () {
+    return JSON.parse(localStorage.getItem("productos")) || [];
 }
 
-// Definiendo elementos a mostrar
-function agregarCarrito (prodID, prodName, prodPrice, prodImg) {
-    let productRow = document.createElement("div");
-    let contenedorRows = document.querySelector(".product-rows");
+function guardarStorage (carritoNuevo) {
+    localStorage.setItem("productos", JSON.stringify(carritoNuevo))
+}
 
-    let productoCarrito = `
-        <div class="prod-row" id="${prodID}">
-            <img class="img-carrito" src="${prodImg}"/>
-            <span>${prodName}</span>
-            <span class="precio-carrito">${prodPrice}</span>
-            <button class="remove-btn">Borrar</button>
-        </div>
-    `
-    productRow.innerHTML = productoCarrito;
-    contenedorRows.append(productRow);
+// Esto creo que debería ir en una función pero se me quemo la cabeza
+carritoSto = capturarStorage()
+carritoSto.forEach(product => {
+    carritoContenedor.innerHTML += 
+        `<div class="prod-row" id="${product.id}">
+            <img class="img-carrito" src="${product.img}"/>
+            <span>${product.nombre}</span>
+            <span class="precio-carrito">$${product.precio}</span>
+            <button id="borrar${product.id}" class="remove-btn">Borrar</button>
+        </div>`
+})
 
-    let botonesBorrar = productRow.querySelectorAll(".remove-btn");
-    for(let boton of botonesBorrar) {
-        boton.addEventListener("click", borrarElemento);
+// -----------------------------------------------------------
+
+const shopCarrito = [];
+
+const carrito = (productId) => {
+    const mostrarEnCarrito = () => {
+        let product = listaProductos.find(product => product.id == productId);
+        shopCarrito.push(product);
+// localStorage set
+        localStorage.setItem("productos", JSON.stringify(shopCarrito));
+// ----------------
+        let div = document.createElement("div");
+        div.innerHTML = 
+        `<div class="prod-row" id="${product.id}">
+            <img class="img-carrito" src="${product.img}"/>
+            <span>${product.nombre}</span>
+            <span class="precio-carrito">$${product.precio}</span>
+            <button id="borrar${product.id}" class="remove-btn">Borrar</button>
+        </div>`
+        carritoContenedor.appendChild(div);
+
+        let botonBorrar = document.getElementById(`borrar${product.id}`);
+        botonBorrar.addEventListener("click", (e) => {
+            borrarProducto(e)
+        })
     }
+
+    mostrarEnCarrito();
 }
 
-// Función de remover elementos
-function borrarElemento(e) {
-    btnBorrar = e.target;
-    btnBorrar.parentElement.parentElement.remove();
+
+function borrarProducto (e) {
+    let botonClick = e.target;
+    botonClick.parentElement.remove();
 }
 
+
+// FALTA HACER QUE BOTON BORRAR TMB BORRE
 // FALTA AGREGAR EL TOTAL!!!!
